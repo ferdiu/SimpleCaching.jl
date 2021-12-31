709be6adb73ba67ec6785d93d82af62c888e8f9e
ferdiu <ferdiu.manzella@gmail.com>
2021-12-31 10:39:50 +0100

    Fixed: type; Added: documentation for @scache and @scachefast macros

--- src/caching.jl
+++ src/caching.jl
@@ -108,7 +108,7 @@ macro _scache(type, common_cache_dir, ex)
 				printcheckpoint(
 					settings.log_output, "Computing " * $(type) * "...";
 					format = settings.date_format,
-					start = setings.line_starter
+					start = settings.line_starter
 				)
 			end
 
@@ -122,7 +122,7 @@ macro _scache(type, common_cache_dir, ex)
 					"Computed " * $(type) * " in " * human_readable_time_s(_finish_time) *
 						" seconds (" * human_readable_time(_finish_time) * ")";
 					format = settings.date_format,
-					start = setings.line_starter
+					start = settings.line_starter
 				)
 			end
 
@@ -141,8 +141,96 @@ macro _scache(type, common_cache_dir, ex)
 	end
 end
 
+const _ext_docs = """!!! note
+
+	The file extension will be `.jld` when using  both [`scachefast`](@ref) and
+	[`scache`](@ref).
+"""
+
 """
 	scache(type, cache_dir, function_call)
+
+Cache the result of `function_call` in directory `cache_dir` prefixing the saved file with
+`type`.
+
+This macro uses `JLD2` `@save` and `@load` macros to save and load cached files so it is
+slower and less memory efficent than [`scachefast`](@ref) macro which uses `serialize`
+which, on the other hand, is less portable between different julia versions.
+
+$_ext_docs
+
+## Examples
+```
+julia> using SimpleCaching
+
+julia> SimpleCaching.settings.log = true
+true
+
+julia> @scache "cute-cube" "./" fill(0, 3, 3, 3)
+● [ 31/12/2021 09:54:08 ] Computing cute-cube...
+● [ 31/12/2021 09:54:08 ] Computed cute-cube in 0.044 seconds (00:00:00)
+● [ 31/12/2021 09:54:13 ] Saving cute-cube to file ./cute-cube_8ad46882688c6820fc0b59db89cfe7f6ca494e3045d7ece8acba1027c4c03c45.jld[.tmp]...
+3×3×3 Array{Int64, 3}:
+[:, :, 1] =
+ 0  0  0
+ 0  0  0
+ 0  0  0
+
+[:, :, 2] =
+ 0  0  0
+ 0  0  0
+ 0  0  0
+
+[:, :, 3] =
+ 0  0  0
+ 0  0  0
+ 0  0  0
+
+julia> @scache "cute-cube" "./" fill(0, 3, 3, 3)
+● [ 31/12/2021 09:54:24 ] Loading cute-cube from file ./cute-cube_8ad46882688c6820fc0b59db89cfe7f6ca494e3045d7ece8acba1027c4c03c45.jld...
+3×3×3 Array{Int64, 3}:
+[:, :, 1] =
+ 0  0  0
+ 0  0  0
+ 0  0  0
+
+[:, :, 2] =
+ 0  0  0
+ 0  0  0
+ 0  0  0
+
+[:, :, 3] =
+ 0  0  0
+ 0  0  0
+ 0  0  0
+
+ shell> ls -lh cute-cube_8ad46882688c6820fc0b59db89cfe7f6ca494e3045d7ece8acba1027c4c03c45.jld
+ -rw-r--r--. 1 user user 1000 31 dic 09.54 cute-cube_8ad46882688c6820fc0b59db89cfe7f6ca494e3045d7ece8acba1027c4c03c45.jld
+```
+
+Expressions in function argument will be computed as first step so the cached file will be
+loaded even if the arguments are different but will evaluate to the same result.
+
+```
+julia> @scache "cute-cube" "./" fill(0, 3, parse(Int64, "3"), 3)
+● [ 31/12/2021 09:54:54 ] Loading cute-cube from file ./cute-cube_8ad46882688c6820fc0b59db89cfe7f6ca494e3045d7ece8acba1027c4c03c45.jld...
+3×3×3 Array{Int64, 3}:
+[:, :, 1] =
+ 0  0  0
+ 0  0  0
+ 0  0  0
+
+[:, :, 2] =
+ 0  0  0
+ 0  0  0
+ 0  0  0
+
+[:, :, 3] =
+ 0  0  0
+ 0  0  0
+ 0  0  0
+
+```
 """
 macro scache(type, common_cache_dir, ex)
 	global _use_serialize = false
@@ -152,6 +240,86 @@ end
 
 """
 	scachefast(type, cache_dir, function_call)
+
+Cache the result of `function_call` in directory `cache_dir` prefixing the saved file with
+`type`.
+
+This macro uses `Serialize` `serialize` and `deserialize` functions to save and load cached
+files so it is faster and more memory efficent than [`scache`](@ref) macro which uses
+`JLD2` which, on the other hand, is more portable between different julia versions.
+
+$_ext_docs
+
+## Examples
+```
+julia> using SimpleCaching
+
+julia> SimpleCaching.settings.log = true
+true
+
+julia> @scachefast "cute-cube" "./" fill(0, 3, 3, 3)
+● [ 31/12/2021 10:31:15 ] Computing cute-cube...
+● [ 31/12/2021 10:31:15 ] Computed cute-cube in 0.042 seconds (00:00:00)
+● [ 31/12/2021 10:31:19 ] Saving cute-cube to file ./cute-cube_8ad46882688c6820fc0b59db89cfe7f6ca494e3045d7ece8acba1027c4c03c45.jld[.tmp]...
+3×3×3 Array{Int64, 3}:
+[:, :, 1] =
+ 0  0  0
+ 0  0  0
+ 0  0  0
+
+[:, :, 2] =
+ 0  0  0
+ 0  0  0
+ 0  0  0
+
+[:, :, 3] =
+ 0  0  0
+ 0  0  0
+ 0  0  0
+
+julia> @scachefast "cute-cube" "./" fill(0, 3, 3, 3)
+● [ 31/12/2021 10:31:23 ] Loading cute-cube from file ./cute-cube_8ad46882688c6820fc0b59db89cfe7f6ca494e3045d7ece8acba1027c4c03c45.jld...
+3×3×3 Array{Int64, 3}:
+[:, :, 1] =
+ 0  0  0
+ 0  0  0
+ 0  0  0
+
+[:, :, 2] =
+ 0  0  0
+ 0  0  0
+ 0  0  0
+
+[:, :, 3] =
+ 0  0  0
+ 0  0  0
+ 0  0  0
+
+```
+
+Expressions in function argument will be computed as first step so the cached file will be
+loaded even if the arguments are different but will evaluate to the same result.
+
+```
+julia> @scachefast "cute-cube" "./" fill(0, 3, round(Int64, 1.5 * 2), 3)
+● [ 31/12/2021 10:32:06 ] Loading cute-cube from file ./cute-cube_8ad46882688c6820fc0b59db89cfe7f6ca494e3045d7ece8acba1027c4c03c45.jld...
+3×3×3 Array{Int64, 3}:
+[:, :, 1] =
+ 0  0  0
+ 0  0  0
+ 0  0  0
+
+[:, :, 2] =
+ 0  0  0
+ 0  0  0
+ 0  0  0
+
+[:, :, 3] =
+ 0  0  0
+ 0  0  0
+ 0  0  0
+
+```
 """
 macro scachefast(type, common_cache_dir, ex)
 	global _use_serialize = true
